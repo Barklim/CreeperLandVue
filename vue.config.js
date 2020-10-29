a96b1edbdd375f81574dd82375dbddf829a7b5ee
Barklim <kliment.barkalov@gmail.com>
2020-10-29 23:48:29 +0300

    gh-pages branch

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/CreeperLand/'
   //   : '/',
+  // prepare gh-pages
   publicPath:'/CreeperLand/',
   pluginOptions: {
     i18n: {
