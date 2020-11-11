7928abc94a9aaf029a85251b3ab84dedb79405a6
Barklim <kliment.barkalov@gmail.com>
2020-11-12 02:12:54 +0300

    fix vue.config.js for deploy vercel

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
   // publicPath: process.env.NODE_ENV === 'production'
   //   ? '/CreeperLand/'
   //   : '/',
-  publicPath:'/CreeperLand/',
+  
+  // publicPath:'/CreeperLand/',
   pluginOptions: {
     i18n: {
       locale: "en",
